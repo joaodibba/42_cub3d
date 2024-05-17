91a8fa89030cf2d8f22e10a64c1dd61b42fd9a87
rajh-phuyal <rajhphuyal44@gmail.com>
2024-05-17 01:04:35 +0100

    added initial raycasting, needs more testing though

--- raycasting/src/main.c
+++ raycasting/src/main.c
@@ -1,12 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rphuyal <rphuyal@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 00:37:36 by rphuyal           #+#    #+#             */
+/*   Updated: 2024/05/17 00:54:22 by rphuyal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/raycast.h"
+
 
 int main(void) {
     t_player player;
+    t_pixel_col pixel_col;
+
+    // configs to test
+    char mapTemp[WINDOW_W][WINDOW_H] = {
+        {1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1}
+    };
+
+    // turn the above map into a 2D array of char pointers
+    char **map = (char **)malloc(WINDOW_W * sizeof(char *));
+    for (int i = 0; i < WINDOW_W; i++)
+        map[i] = mapTemp[i];
+
+    // initialize player position and direction
+    player.pos.x = 2;
+    player.pos.y = 2;
+    player.dir.x = -1;
+    player.dir.y = 0;
+    player.plane.x = 0;
+    player.plane.y = 0.66;
 
     int pixel_count = -1;
-    while (++pixel_count < WINDOW_W)
-    {
-        raycast(NULL, &player);
+    while (++pixel_count < WINDOW_W) {
+        raycast(map, &player, pixel_count, NULL);
     }
-    
 }
--- raycasting/src/raycast.c
+++ raycasting/src/raycast.c
@@ -1,21 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycast.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rphuyal <rphuyal@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 00:43:44 by rphuyal           #+#    #+#             */
+/*   Updated: 2024/05/17 01:01:15 by rphuyal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/raycast.h"
 
-int     dda(void)
-{
-    // return which side was hit
-    return (0);
+static void __dda(t_computes *computes, char **map) {
+
+	computes->hit = false;
+	while (true) {
+		// jump to the nearest side
+		if (computes->side_dist.x < computes->side_dist.y) {
+			computes->side_dist.x += computes->delta.x;
+			computes->map.x += computes->step.x;
+			computes->side = SIDE_X;
+		}
+		else {
+			computes->side_dist.y += computes->delta.y;
+			computes->map.y += computes->step.y;
+			computes->side = SIDE_Y;
+		}
+
+		if (map[computes->map.x][computes->map.y] > 0) {
+			computes->hit = true;
+			break ;
+		}
+	}
 }
 
-void    raycast(char **map, t_player *player)
-{
-    char mapTemp[6][6] = {
-        {1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 1},
-        {1, 0, 0, 'P', 0, 1},
-        {1, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1}
-    };
+static void	__sides(t_computes *computes, t_player *player) {
+	if (computes->ray.x < 0) {
+		computes->step.x = -1;
+		computes->side_dist.x = (player->pos.x - computes->map.x) * computes->delta.x;
+	}
+	else {
+		computes->step.x = 1;
+		computes->side_dist.x = ((computes->map.x + 1.0) - player->pos.x) * computes->delta.x;
+	}
+	if (computes->ray.y < 0) {
+		computes->step.y = -1;
+		computes->side_dist.y = (player->pos.y - computes->map.y) * computes->delta.y;
+	}
+	else {
+		computes->step.y = 1;
+		computes->side_dist.y = ((computes->map.y + 1.0) - player->pos.y) * computes->delta.y;
+	}
+}
 
-    return ;
+static void	__deltas(t_computes *computes) {
+	int	guard_x;
+	int	guard_y;
+
+	// used for shortcurcit
+	guard_x = computes->ray.x > 0;
+	guard_y = computes->ray.y > 0;
+
+	// compute the deltas if the ray is pos, else a positive value
+	computes->delta.x = (guard_x * fabs(1 / computes->ray.x)) + (!guard_x * 1e30);
+	computes->delta.y = (guard_y * fabs(1 / computes->ray.y)) + (!guard_y * 1e30);
+}
+
+static void	__pos_and_dir(t_computes *computes, t_player *player, double camera) {
+
+	// ray starts at the player's position, not the camera
+	// so we need to get the player's position in the map, which is a square
+	computes->map.x = (int)player->pos.x;
+	computes->map.y = (int)player->pos.y;
+
+	// get the camera plane ops straight from the eye of the player
+	computes->ray.x = player->dir.x + (player->plane.x * camera);
+	computes->ray.y = player->dir.y + (player->plane.y * camera);
+}
+
+t_computes *raycast(char **map, t_player *player, int column, t_computes *computes) {
+
+	double		camera;
+	t_computes	*_computes;
+
+	_computes = computes;
+	// allocate a compute var if not provided
+	if (!_computes) {
+		_computes = malloc(sizeof(t_computes));
+		if (!_computes) {
+			// TODO: later change it to :
+			// ft_putendl_fd("Memory allocation failure! Aborting.", 2);
+			printf("Memory allocation failure! Aborting.");
+			return (NULL);
+		}
+	}
+	// get where the in the x-cordinate the camera plane is
+	camera = (2 * (column / (double)WINDOW_W)) - 1;
+	__pos_and_dir(_computes, player, camera);
+	__deltas(computes);
+	__sides(computes, player);
+	__dda(_computes, map);
+
+	return _computes;
 }
