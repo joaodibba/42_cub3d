48ccca1c5c66c6cac54524ac57810b5e7860ff92
Raj Phuyal <rphuyal@c1r6s2.42lisboa.com>
2024-05-18 19:24:40 +0100

    transfer

--- raycasting/src/helpers.c
+++ raycasting/src/helpers.c
@@ -6,8 +6,16 @@
 /*   By: rphuyal <rphuyal@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 01:50:46 by rphuyal           #+#    #+#             */
-/*   Updated: 2024/05/17 11:08:03 by rphuyal          ###   ########.fr       */
+/*   Updated: 2024/05/18 17:19:59 by rphuyal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/raycast.h"
+
+void    __wall_distance(t_computes *computes) {
+    
+}
+
+void    __side_color(t_computes *computes) {
+    
+}
