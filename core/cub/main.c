3107150f27a4fc95509466e0e14da10f6b8553af
joaodibba <joaomiranda.pessoal@gmail.com>
2024-06-09 20:25:03 +0100

    a lot of changes, rajh is going to merge

--- core/cub/main.c
+++ core/cub/main.c
@@ -20,68 +20,6 @@ static bool	guard(int ac, char **av)
 	return (false);
 }
 
-static bool initialization(t_window **win, t_map **map)
-{
-	*win = (t_window *)malloc(sizeof(t_window));
-	if (!*win)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to allocate memory for window.\n");
-		return (false);
-	}
-	(*win)->mlx = mlx_init();
-	if (!(*win)->mlx)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to initialize mlx.\n");
-		free(*win);
-		return (false);
-	}
-	(*win)->win = mlx_new_window((*win)->mlx, WIN_WIDTH, WIN_HEIGHT, "cub3D");
-	if (!(*win)->win)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to create window.\n");
-		free(*win);
-		return (false);
-	}
-
-	*map = (t_map *)malloc(sizeof(t_map));
-	if (!*map)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to allocate memory for map.\n");
-		free(*win);
-		return (false);
-	}
-	**map = (t_map){ .map = NULL, .floor = 0, .ceiling = 0, .no = NULL, .so = NULL, .we = NULL, .ea = NULL };
-	(*win)->img = (t_image *)malloc(sizeof(t_image));
-	if (!(*win)->img)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to allocate memory for image.\n");
-		free(*win);
-		free(*map);
-		return (false);
-	}
-	(*win)->img->img = mlx_new_image((*win)->mlx, WIN_WIDTH, WIN_HEIGHT);
-	if (!(*win)->img->img)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to create image.\n");
-		free(*win);
-		free(*map);
-		free((*win)->img);
-		return (false);
-	}
-	(*win)->img->addr = mlx_get_data_addr((*win)->img->img, &(*win)->img->bpp, &(*win)->img->line_len, &(*win)->img->endian);
-	if (!(*win)->img->addr)
-	{
-		ft_fprintf(STDERR_FILENO, "Error: Failed to get image address.\n");
-		free(*win);
-		free(*map);
-		free((*win)->img);
-		return (false);
-	}
-	(*win)->img->width = WIN_WIDTH;
-	(*win)->img->height = WIN_HEIGHT;
-	return (true);
-}
-
 int cube_loop(t_cub *cub)
 {
 	player_move(&cub->player, cub->ctrl, cub->map->map);
@@ -91,6 +29,8 @@ int cube_loop(t_cub *cub)
 	mlx_put_image_to_window(cub->win->mlx, cub->win->win, cub->win->img->img, 0, 0);
 	return (0);
 }
+
+bool initialization(t_window **win, t_map **map);
 
 int main(int argc, char **argv)
 {
